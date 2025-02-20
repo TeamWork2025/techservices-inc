80107abe2cf9ed7a55c4d8f07cfbccb62a4f8561
Adnaan-XVII <adnaanhoosen51@gmail.com>
2025-02-20 13:50:28 +0200

    Add files via upload

---
+++ services.js
@@ -0,0 +1,15 @@
+// Smooth scrolling effect
+const navLinks = document.querySelectorAll('a[href^="#"]');
+navLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
